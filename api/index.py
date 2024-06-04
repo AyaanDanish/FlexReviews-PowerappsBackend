754d7ec81d65c547465ce0120b396e710d2ab3b7
AyaanDanish <ayaandanish7@gmail.com>
2024-06-04 15:56:09 +0500

    Added sentiment analyzer

--- api/index.py
+++ api/index.py
@@ -1,13 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
 
 app = Flask(__name__)
-
-
-@app.route("/test", methods=["GET"])
-def test_endpoint():
-    return "Hello, this is a test endpoint!", 200
 
 
 @app.route("/", methods=["GET"])
 def homepage():
     return render_template("index.html")
+
+
+def preprocess_text(text):
+    # Tokenize the text
+    tokens = word_tokenize(text.lower())
+
+    # Remove stop words
+    filtered_tokens = [
+        token for token in tokens if token not in stopwords.words("english")
+    ]
+
+    # Lemmatize the tokens
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+    # Join the tokens back into a string
+    processed_text = " ".join(lemmatized_tokens)
+    return processed_text
+
+
+# initialize NLTK sentiment analyzer
+analyzer = SentimentIntensityAnalyzer()
+
+
+# create get_sentiment function
+def get_sentiment(text):
+    scores = analyzer.polarity_scores(text)
+    sentiment = "Positive" if scores["pos"] > 0 else "Negative"
+    return sentiment
+
+
+@app.route("/sentiment", methods=["POST"])
+def sentiment():
+    text = request.form.get("text")
+    return get_sentiment(text)
